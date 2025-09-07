d6d87071315f6015489930cee83db5a1cd55cdd7
ennn05 <joshuakce1201@gmail.com>
2025-09-07 23:56:48 +0800

    Create apps.jsx

---
+++ a2/ frontend/src/styles/apps.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Lessons from "./pages/Lessons";
+import Courses from "./pages/Courses";
+import CourseDetails from "./pages/CourseDetails";
+import CreateCourse from "./pages/CreateCourse";
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/lessons" element={<Lessons />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/courses/:courseId" element={<CourseDetails />} />
+        <Route path="/courses/create" element={<CreateCourse />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
